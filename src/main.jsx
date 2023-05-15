3d5d28f533547abd08b9916f26f0345acd8ad4cc
spencer.trinh <spencer.trinh@kinnate.com>
2023-05-15 11:37:14 -0700

    app version tag

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AppVersion from './AppVersionTag'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <AppVersion />
   </React.StrictMode>
 )
